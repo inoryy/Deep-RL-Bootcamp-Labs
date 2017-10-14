122f7fd3a50d1ba08a043375323055d4fd5b0bd9
Roman Ring <inoryy@gmail.com>
2017-10-14 13:32:17 +0300

    minor fixes to get prelab working

--- prelab/scripts/test_environment_setup.py
+++ prelab/scripts/test_environment_setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 """
 This project was developed by Rocky Duan, Peter Chen, Pieter Abbeel for the Berkeley Deep RL Bootcamp, August 2017. Bootcamp website with slides and lecture videos: https://sites.google.com/view/deep-rl-bootcamp/.
 
@@ -10,11 +12,6 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 """
-
-
-
-#!/usr/bin/env python
-
 
 def main():
     import roboschool
